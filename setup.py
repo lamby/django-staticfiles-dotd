1abc6ad971bd75c00d9fba40b44043841ea12683
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-20 14:39:49 +0800

    Releasing version 1.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='django-staticfiles-dotd',
 
     url="https://chris-lamb.co.uk/projects/django-staticfiles-dotd",
-    version='1.2.1',
+    version='1.3.0',
     description="Django staticfiles adaptor to concatentate .d-style"
         " directories",
 
